b9ef9a608264c80e4fa0eb63d23e046dc501c4f8
roboserg <shebotnov@gmail.com>
2026-03-25 22:53:26 +0100

    Auto-clean Release folder after build
    Remove intermediate build artifacts (obj, pch, pdb files) after successful build.

--- build.ps1
+++ build.ps1
@@ -2,3 +2,8 @@ $msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\MSBuild
 $config = if ($args[0]) { $args[0] } else { "Release" }
 
 & $msbuild TheHeaven.sln /p:Configuration=$config /p:Platform=Win32
+
+# Clean up intermediate build artifacts
+if (Test-Path "Release") {
+    Remove-Item -Recurse -Force "Release"
+}
